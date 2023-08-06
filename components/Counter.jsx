4ede859e10116225490decc945cef2bc6b056088
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-08-06 13:38:07 +0530

    counter redux store completed

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,12 +1,20 @@
+"use client";
+
+
+import { decrement, increment } from '@redux/features/counter/counterSlice';
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 function Counter() {
+    const count = useSelector(state => state.counter.count);
+    const dispatch = useDispatch();
   return (
     <div>
-        <h3 className="text-center py-3 text-xl">Count is : 0</h3>
+        <h3 className="text-center py-3 text-xl">Count is : {count}</h3>
         <div className="flex justify-center gap-3">
-            <button className="py-3 px-6 bg-slate-400 rounded-sm hover:bg-slate-600 hover:text-gray-200">Increase Count</button>
-            <button className="py-3 px-6 bg-slate-400 rounded-sm hover:bg-slate-600 hover:text-gray-200">Decrease Count</button>
+            <button onClick={() => dispatch(increment())} className="py-3 px-6 bg-slate-400 rounded-sm hover:bg-slate-600 hover:text-gray-200">Increase Count</button>
+            <button onClick={() => dispatch(decrement())} className="py-3 px-6 bg-slate-400 rounded-sm hover:bg-slate-600 hover:text-gray-200">Decrease Count</button>
         </div>
     </div>
   )
